bdbeae7a66631c115f0eb5b33eb654702437712b
Matheus Feres Turcheti <71662383+matheus-ft@users.noreply.github.com>
2022-06-21 17:31:43 -0300

    Organize data

--- main.m
+++ main.m
@@ -1,4 +1,4 @@
-img_path = 'java.jpg';
+img_path = "photos/java.jpg";
 A = imread(img_path);
 G = A(:, :, 2);
 
@@ -6,17 +6,17 @@ r = rank(G);
 printf("The original photograph is given by a matrix with rank %d.\n", r);
 
 M0 = svd_img(A, 1);
-imwrite(M0, strcat("recovered_r1.png"));
+imwrite(M0, strcat("photos/recovered_r1.png"));
 
 M1 = svd_img(A, 51);
-imwrite(M1, strcat("recovered_r51.png"));
+imwrite(M1, strcat("photos/recovered_r51.png"));
 
 M2 = svd_img(A, 101);
-imwrite(M2, strcat("recovered_r101.png"));
+imwrite(M2, strcat("photos/recovered_r101.png"));
 
 M3 = svd_img(A, 151);
-imwrite(M3, strcat("recovered_r151.png"));
+imwrite(M3, strcat("photos/recovered_r151.png"));
 
 M4 = svd_img(A, 201);
-imwrite(M4, strcat("recovered_r201.png"));
+imwrite(M4, strcat("photos/recovered_r201.png"));
 
