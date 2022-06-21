7d0ef272429617f8e57770b364344d1eef00cb41
Matheus Feres Turcheti <71662383+matheus-ft@users.noreply.github.com>
2022-06-21 17:25:48 -0300

    Fix svd_img function

--- svd_img.m
+++ svd_img.m
@@ -1,12 +1,9 @@
 function M = svd_img(A, r)
     % Approximate, via Singular Value Decomposition, an RGB matrix by another with lower rank.
     % A (n by m by 3 matrix) original RGB matrix
-    % r (integer) custom rank of the approximated matrix - if 0, interpreted as the original rank
+    % r (integer) custom rank of the approximated matrix
     % M (n by m by 3 matrix) approximated RGB matrix
     [R, G, B] = rgb(A);
-    if (r == 0)
-        r = rank(G);  % rank(R) == rank(G) == rank(B)
-    end
     R = approximate(R, r);
     G = approximate(G, r);
     B = approximate(B, r);
@@ -14,14 +11,14 @@ function M = svd_img(A, r)
 end
 
 function [R, G, B] = rgb(A)
-    R = A(:, :, 1);
-    G = A(:, :, 2);
-    B = A(:, :, 3);
+    R = im2double(A(:, :, 1));
+    G = im2double(A(:, :, 2));
+    B = im2double(A(:, :, 3));
 end
 
 function M = approximate(A, r)
     [U, S, V] = svd(A);
-    M = U(:, 1:r) * S(1:r, 1:r) * V'(1:r, :);
+    M = U(:, 1:r) * S(1:r, 1:r) * (V(:, 1:r))';
 end
 
 function M = picture(R, G, B)
