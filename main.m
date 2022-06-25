1e2798c498145bb6bc4890251dbad18d5890728a
Matheus Feres Turcheti <71662383+matheus-ft@users.noreply.github.com>
2022-06-24 21:27:26 -0300

    Add 21 rank recovery

--- main.m
+++ main.m
@@ -6,17 +6,20 @@ r = rank(G);
 printf("The original photograph is given by a matrix with rank %d.\n", r);
 
 M0 = svd_img(A, 1);
-imwrite(M0, strcat("photos/recovered_r1.png"));
+imwrite(M0, "photos/recovered_r1.png");
 
-M1 = svd_img(A, 51);
-imwrite(M1, strcat("photos/recovered_r51.png"));
+M1 = svd_img(A, 21);
+imwrite(M1, "photos/recovered_r21.png");
 
-M2 = svd_img(A, 101);
-imwrite(M2, strcat("photos/recovered_r101.png"));
+M2 = svd_img(A, 51);
+imwrite(M2, "photos/recovered_r51.png");
 
-M3 = svd_img(A, 151);
-imwrite(M3, strcat("photos/recovered_r151.png"));
+M3 = svd_img(A, 101);
+imwrite(M3, "photos/recovered_r101.png");
 
-M4 = svd_img(A, 201);
-imwrite(M4, strcat("photos/recovered_r201.png"));
+M4 = svd_img(A, 151);
+imwrite(M4, "photos/recovered_r151.png");
+
+M5 = svd_img(A, 201);
+imwrite(M5, "photos/recovered_r201.png");
 
